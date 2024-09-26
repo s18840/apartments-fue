15c12b6a7abde00e693e059d19e134bcdd808589
s18840 <Mikoaj@DESKTOP-G6E9EB5>
2024-09-26 16:52:30 +0200

    replaced footer mail and phone

--- apartments-fue/src/components/Footer.js
+++ apartments-fue/src/components/Footer.js
@@ -10,17 +10,13 @@ function Footer() {
         <div className="contact-us">{t("Contact_us")}</div>
         <div>{t("Host")}</div>
         <div className="footer-text">
-          {t("Mail")}: 
-          <div>
-          fuerteventura@gmail.com
-          </div>
-
+          {t("Mail")}:
+          <a href="mailto:coloresdefuerteventura@gmail.com">
+            coloresdefuerteventura@gmail.com
+          </a>
         </div>
         <div className="footer-text">
-          {t("Telephone")}:
-          <div>
-            +48
-            </div>
+          {t("Telephone")}:<div>+34 697 30 89 89</div>
         </div>
       </div>
     </div>
